5715907beab354c230be7c1c1a7859238a3bf7da
PasqualeIannace <pasquale.iannace@gmail.com>
2023-12-15 15:15:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    
+    <?php $nome = "Ciao mondo" ?>
+    <h1><?php echo $nome . " esempio 1" ?></h1>
+
+
+    <?php $nome = "Nuovo mondo" ?>
+    <h2><?php echo " $nome esempio 2 " ?></h2>
+
+
+
+</body>
+</html>
